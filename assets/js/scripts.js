b4d80e478421ae2d9821f9cd6ff1cb7a6c63358c
Sanjay Mahto <mahto.sanjay41@gmail.com>
2018-11-01 00:30:48 +0530

    index file updated.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -46,18 +46,18 @@ jQuery(document).ready(function() {
     new WOW().init();
 });
 
-jQuery(window).load(function() {
+// jQuery(window).load(function() {
 	
-	/*
-		Loader
-	*/
-	$(".loader-img").fadeOut();
-	$(".loader").delay(1000).fadeOut("slow");
+// 	/*
+// 		Loader
+// 	*/
+// 	$(".loader-img").fadeOut();
+// 	$(".loader").delay(1000).fadeOut("slow");
 	
-	/*
-		Hidden images
-	*/
-	$(".testimonial-image img").attr("style", "width: auto !important; height: auto !important;");
+// 	/*
+// 		Hidden images
+// 	*/
+// 	$(".testimonial-image img").attr("style", "width: auto !important; height: auto !important;");
 	
-});
+// });
 
